5c645a5f0a2d5c96bb8eff6d4b0e16e729c473ed
Michael Wilson <michael.wilson@nelnet.net>
2021-06-23 09:19:13 -0500

    Number manipulateor changes

---
+++ NumberManipulator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NumberManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num * num + 14 - 3);
+        }
+    }
+}
